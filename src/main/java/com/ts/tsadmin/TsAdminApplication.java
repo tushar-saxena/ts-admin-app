c2d9ad7eaf4d0ffbfbe4a3aa8d9af29b9da2faa2
Ajay Kumar <ajay.ishan06@gmail.com>
2019-10-05 02:16:33 +0530

    Created jar file

--- src/main/java/com/ts/tsadmin/TsAdminApplication.java
+++ src/main/java/com/ts/tsadmin/TsAdminApplication.java
@@ -1,13 +1,23 @@
 package com.ts.tsadmin;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.context.TypeExcludeFilter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan(
+        basePackageClasses = {TsAdminApplication.class},
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+                @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class)
+        })
+
 public class TsAdminApplication {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SpringApplication.run(TsAdminApplication.class, args);
-    }
+    }*/
 
 }
